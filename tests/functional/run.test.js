7a060ca194bf2683c16f15cea24f8aa765d166d2
tony-go <gorez.tony@gmail.com>
2022-04-28 13:50:18 +0200

    test(func): add timeout tests

--- tests/functional/run.test.js
+++ tests/functional/run.test.js
@@ -142,6 +142,32 @@ test('.run should throw error when a act fail but was not expected to fail', asy
   t.end();
 });
 
+test('.run should succeed if timeout are not elapsed', async (t) => {
+  const kCustomTimeoutCommand = 'bash ./tests/functional/fixtures/timeout.sh';
+  const name = 'hali';
+  const scenario = clix(kCustomTimeoutCommand)
+    .expect('Hello, who am I talking to?')
+    .input(name)
+    .expect('Hey ' + name + '!', { timeout: 3_000 });
+
+  const res = await scenario.run();
+
+  t.ok(res.ok);
+  t.end();
+});
+
+test('.run should failed of timeout are elapsed', async (t) => {
+  const kCustomTimeoutCommand = 'bash ./tests/functional/fixtures/timeout.sh';
+  const name = 'hali';
+  const scenario = clix(kCustomTimeoutCommand)
+    .expect('Hello, who am I talking to?')
+    .input(name)
+    .expect('Hey ' + name + '!', { timeout: 1_000 });
+
+  await t.rejects(scenario.run());
+  t.end();
+});
+
 /**
  * HELPERS
  */
