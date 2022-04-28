530499cfa96d5a37a2678c48e50a03d0ba8a89d5
tony-go <gorez.tony@gmail.com>
2022-04-28 17:40:49 +0200

    test(func): add timeout tests for expect error

--- tests/functional/run.test.js
+++ tests/functional/run.test.js
@@ -142,7 +142,7 @@ test('.run should throw error when a act fail but was not expected to fail', asy
   t.end();
 });
 
-test('.run should succeed if timeout are not elapsed', async (t) => {
+test('.run should succeed if expect timeout are not elapsed', async (t) => {
   const kCustomTimeoutCommand = 'bash ./tests/functional/fixtures/timeout.sh';
   const name = 'hali';
   const scenario = clix(kCustomTimeoutCommand)
@@ -156,13 +156,41 @@ test('.run should succeed if timeout are not elapsed', async (t) => {
   t.end();
 });
 
-test('.run should failed of timeout are elapsed', async (t) => {
+test('.run rejects failed if expect timeout are elapsed', async (t) => {
   const kCustomTimeoutCommand = 'bash ./tests/functional/fixtures/timeout.sh';
   const name = 'hali';
   const scenario = clix(kCustomTimeoutCommand)
     .expect('Hello, who am I talking to?')
     .input(name)
     .expect(`Hey ${name}!`, { timeout: 1_000 });
+
+  await t.rejects(scenario.run());
+  t.end();
+});
+
+test('.run should succeed if expectError timeout are not elapsed', async (t) => {
+  const kCustomTimeoutCommand =
+    'bash ./tests/functional/fixtures/timeout-error.sh';
+  const name = 'hali';
+  const scenario = clix(kCustomTimeoutCommand)
+    .expect('Hello, who am I talking to?')
+    .input(name)
+    .expectError('Hey ' + name + '!', { timeout: 3_000 });
+
+  const res = await scenario.run();
+
+  t.ok(res.ok);
+  t.end();
+});
+
+test('.run should rejects if expectError timeout are elapsed', async (t) => {
+  const kCustomTimeoutCommand =
+    'bash ./tests/functional/fixtures/timeout-error.sh';
+  const name = 'hali';
+  const scenario = clix(kCustomTimeoutCommand)
+    .expect('Hello, who am I talking to?')
+    .input(name)
+    .expectError('Hey ' + name + '!', { timeout: 1_000 });
 
   await t.rejects(scenario.run());
   t.end();
